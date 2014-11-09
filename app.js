77e816999cc07b3ebb1e248c258e8b3a88f9f346
soar <85624529@qq.com>
2014-11-09 18:28:39 +0800

    first init

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require('http'),
+    faye = require('faye');
+
+var server = http.createServer(),
+    bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
+
+bayeux.on('handshake', function(clientId) {
+   console.log("client connected: "+ clientId);
+   send("/chat","welcome");
+});
+
+bayeux.on('publish', function(clientId,channel,data) {
+   console.log("a message incoming, from: "+ clientId+ "   channel:"+channel + "   data:" + data.text );
+   //send("chat","welcome");
+});
+
+bayeux.attach(server);
+server.listen(8000);
+
+function send(channel, text){
+  bayeux.getClient().publish(channel, {
+     text:   text
+  });
+}
